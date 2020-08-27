c1312b4138d1622d4291811aac25d6c10f1d7928
hackrhive <rawalchirag2000@gmail.com>
2020-08-28 01:02:24 +0530

    optimized for multiple purpose

--- lib/profileScreen.dart
+++ lib/profileScreen.dart
@@ -1,10 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'Helper/OfflineStore.dart';
 import 'auth.dart';
 
 class ProfileScreen extends StatefulWidget {
+  Map<String, dynamic> userData;
+  ProfileScreen({this.userData});
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
@@ -22,94 +23,116 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: offlineStorage.getUserInfo(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            Map<String, String> user = snapshot.data;
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundImage: NetworkImage(
-                      user['photo'],
+      body: (widget.userData == null)
+          ? FutureBuilder(
+              future: offlineStorage.getUserInfo(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  Map<String, String> user = snapshot.data;
+                  return Center(
+                    child: Container(
+                      height: MediaQuery.of(context).size.width * 0.8,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Card(
+                        margin: EdgeInsets.all(8.0),
+                        elevation: 8.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundImage: NetworkImage(
+                                  user['photo'],
+                                ),
+                              ),
+                              Text(
+                                user['name'],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text(
+                                user['email'],
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                              MaterialButton(
+                                onPressed: () => authService.signOut(),
+                                child: Text('Signout'),
+                                textColor:
+                                    Theme.of(context).colorScheme.onSecondary,
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                } else
+                  return Center(
+                    child: Text('Loading Profile...'),
+                  );
+              },
+            )
+          : Center(
+              child: Container(
+                height: MediaQuery.of(context).size.width * 0.8,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Card(
+                  margin: EdgeInsets.all(8.0),
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundImage: NetworkImage(
+                            widget.userData['photo'].toString(),
+                          ),
+                        ),
+                        Text(
+                          widget.userData['name'].toString(),
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          widget.userData['email'].toString(),
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        MaterialButton(
+                          onPressed: () => authService.signOut(),
+                          child: Text('Signout'),
+                          textColor: Theme.of(context).colorScheme.onSecondary,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        FloatingActionButton(
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Theme.of(context).colorScheme.onSecondary,
+                          ),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ],
                     ),
                   ),
-                  Text(
-                    user['name'],
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  Text(
-                    user['email'],
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
-                  ),
-                  MaterialButton(
-                    onPressed: () => authService.signOut(),
-                    child: Text('Signout'),
-                    textColor: Colors.black,
-                    color: Colors.red,
-                  ),
-                ],
+                ),
               ),
-            );
-          } else
-            return Center(
-              child: Text('Loading Profile...'),
-            );
-        },
-      ),
-    );
-    return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.currentUser().asStream(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            FirebaseUser user = snapshot.data;
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundImage: NetworkImage(
-                      user.photoUrl.toString(),
-                    ),
-                  ),
-                  Text(
-                    user.displayName.toString(),
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  Text(
-                    user.email.toString(),
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
-                  ),
-                  MaterialButton(
-                    onPressed: () => authService.signOut(),
-                    child: Text('Signout'),
-                    textColor: Colors.black,
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            );
-          } else
-            return Center(
-              child: Text('Loading Profile...'),
-            );
-        },
-      ),
+            ),
     );
   }
 }
