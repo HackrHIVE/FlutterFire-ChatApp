267d0cc296d1222ebb3b03b49242512b5205e10d
hackrhive <rawalchirag2000@gmail.com>
2020-09-01 18:02:48 +0530

    image viewer

---
+++ lib/imageViewer.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ImageViewer extends StatefulWidget {
+  String filePath;
+  ImageViewer(this.filePath);
+  @override
+  _ImageViewerState createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Hero(
+          tag: widget.filePath,
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.network(widget.filePath),
+          ),
+        ),
+      ),
+    );
+  }
+}
