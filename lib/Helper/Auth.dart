5ec64e5a48be4f4145bf5eb737dda4b9cd717e2b
hackrhive <rawalchirag2000@gmail.com>
2020-09-01 18:01:00 +0530

    _

--- lib/Helper/Auth.dart
+++ lib/Helper/Auth.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rxdart/rxdart.dart';
 
-import 'Helper/OfflineStore.dart';
+import 'OfflineStore.dart';
 
 class AuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
